6a10d6dc53d2212febd48a37ac118c4368bc15ee
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-09-20 11:04:50 +0200

    Chg: IP and Port Fix: TileMapLayer API

--- scenes/autoloads/Multihelper.gd
+++ scenes/autoloads/Multihelper.gd
@@ -41,7 +41,7 @@ func join_game(address = ""):
 	var peer = WebSocketMultiplayerPeer.new()
 	var error
 	if OS.has_feature("editor"):
-		error = peer.create_client("ws://" + address + ":8443")
+		error = peer.create_client("ws://" + DEFAULT_SERVER_IP + ":"+ str(PORT))
 	else:
 		if Constants.USE_SSL:
 			var tlsOptions = TLSOptions.client()
@@ -241,7 +241,7 @@ func spawnPlayer(playerName, id, characterFile):
 				if y < 0 or y >= map.map_height: continue
 				
 				var pos = Vector2i(x, y)
-				var atlas_coords = map.tile_map.get_cell_atlas_coords(0, pos, false)  # layer 0, position, alternative=false
+				var atlas_coords = map.tile_map.get_cell_atlas_coords(pos)  # layer 0, position, alternative=false
 				
 				# Only spawn on grass tiles
 				if map.grassAtlasCoords.has(atlas_coords):
@@ -251,7 +251,7 @@ func spawnPlayer(playerName, id, characterFile):
 						for dy in range(-2, 3):
 							var check_pos = Vector2i(x + dx, y + dy)
 							if check_pos.x >= 0 and check_pos.x < map.map_width and check_pos.y >= 0 and check_pos.y < map.map_height:
-								var check_coords = map.tile_map.get_cell_atlas_coords(0, check_pos, false)  # layer 0, position, alternative=false
+								var check_coords = map.tile_map.get_cell_atlas_coords(check_pos)  # layer 0, position, alternative=false
 								if map.waterCoors.has(check_coords):
 									is_safe = false
 									break
