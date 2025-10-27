828c5ce9751178cada659dbef6fb1e097b23b6ea
D. Buck <d.buck@hi-gruppe.de>
2025-10-27 16:54:22 +0100

    Chg: Debugging Global Player Position for wrong Map Position
    (cherry picked from commit 9f5c2a85153041203131cf31791cd434c9666b46)

--- scenes/ui/minimap/PointsDraw.gd
+++ scenes/ui/minimap/PointsDraw.gd
@@ -14,15 +14,20 @@ func _ready():
 
 func _process(_delta):
 	queue_redraw()
+	pass
 
 func _draw():
 	if !tile_map or !tile_map.tile_set:
 		return
 		
 	if is_instance_valid(player):
-		var player_pos = player.global_position / Vector2(tile_map.tile_set.tile_size) * tile_size
-		var player_rect = Rect2(player_pos, tile_size*4)
+		#PlayerGlobalPosition
+		var pgp = player.global_position 
+		#TileSetTileSize
+		var tsts = Vector2(tile_map.tile_set.tile_size)
+		var PlayerPosToMinimap = pgp / tsts #* tile_size
+		var player_rect = Rect2(PlayerPosToMinimap, tile_size)
 		draw_rect(player_rect, PLAYER_COLOR)
-		coordsLabel.text = str(Vector2i(player_pos))
+		coordsLabel.text = str(Vector2i(PlayerPosToMinimap))
 	else:
 		player = get_node_or_null("../../../../../Players/"+str(multiplayer.get_unique_id()))
