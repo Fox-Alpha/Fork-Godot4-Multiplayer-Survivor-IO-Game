41290a51a950d4f7926a52959c5a0c493d160969
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-10-29 16:50:21 +0100

    Add: Connect child signal for nodes entered tree ===: preparing not registration for easier global access

--- scenes/game/Game.gd
+++ scenes/game/Game.gd
@@ -1,6 +1,9 @@
 # godot 4.3
 extends Node
 
+func _ready() -> void:
+	pass
+
 func start_game():
 	# Hide the UI and unpause to start the game.
 	%MainMenu.queue_free()
@@ -9,7 +12,7 @@ func start_game():
 	# Clients will instantiate the level via the spawner.
 	if multiplayer.is_server() and not OS.has_environment("dedicated_server"):
 		change_level.call_deferred(load("res://scenes/main/main.tscn"))
-		
+
 # Call this function deferred and only on the main authority (server).
 func change_level(scene: PackedScene):
 	# Remove old level if any.
@@ -17,5 +20,11 @@ func change_level(scene: PackedScene):
 	for c in level.get_children():
 		level.remove_child(c)
 		c.queue_free()
+	var s = scene.instantiate()
+	s.child_entered_tree.connect(_On_ChildEnteredTree)
 	# Add new level.
-	level.add_child(scene.instantiate())
+	level.add_child(s)
+
+
+func _On_ChildEnteredTree(node : Node) -> void:
+	print("Game::_On_ChildEnteredTree() => ", node.name)
