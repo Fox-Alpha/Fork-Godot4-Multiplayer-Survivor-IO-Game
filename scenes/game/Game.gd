497e0bd9bc1d351119a77fb131731d0b04598e37
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-10-31 00:34:11 +0100

    Chg: Adding level to Tree ===: Refactor func Add: signal for child entered tree, to get child nodes for global registration

--- scenes/game/Game.gd
+++ scenes/game/Game.gd
@@ -1,6 +1,10 @@
 # godot 4.3
 extends Node
 
+@onready var mainlevel = preload("res://scenes/main/main.tscn")
+
+@onready var levelnode = %Level
+
 func _ready() -> void:
 	pass
 
@@ -11,20 +15,29 @@ func start_game():
 	# Only change level on the server.
 	# Clients will instantiate the level via the spawner.
 	if multiplayer.is_server() and not OS.has_environment("dedicated_server"):
-		change_level.call_deferred(load("res://scenes/main/main.tscn"))
+		change_level.call_deferred()
 
 # Call this function deferred and only on the main authority (server).
-func change_level(scene: PackedScene):
+func change_level():
 	# Remove old level if any.
-	var level = %Level
-	for c in level.get_children():
-		level.remove_child(c)
-		c.queue_free()
-	var s = scene.instantiate()
-	s.child_entered_tree.connect(_On_ChildEnteredTree)
-	# Add new level.
-	level.add_child(s)
+	for child in levelnode.get_children():
+		child.queue_free()
+
+	if mainlevel.can_instantiate():
+		levelnode.child_entered_tree.connect(_On_LevelEnteredTree)
+		var ml = mainlevel.instantiate()
+		ml.child_entered_tree.connect(_On_ChildEnteredTree)
+		levelnode.call_deferred("add_child", ml)
+		await get_tree().process_frame
+		#levelnode.add_child().call_deferred()
+		#levelnode.get_child(0).child_entered_tree.connect(_On_ChildEnteredTree)
 
 
 func _On_ChildEnteredTree(node : Node) -> void:
-	print("Game::_On_ChildEnteredTree() => ", node.name)
+	if node is Node2D:
+		print("Game::_On_ChildEnteredTree() => ", node.name)
+
+
+func _On_LevelEnteredTree(node : Node) -> void: 
+	print("Game::_On_LevelEnteredTree() => ", node.name)
+	pass
