21766151c2d565244f85e97646ec80d871b440c5
Fox-Alpha <fox-alpha@tarnhoerner.de>
2025-11-04 17:08:35 +0100

    Chg: Test of feature instead of enviroment

--- scenes/game/Game.gd
+++ scenes/game/Game.gd
@@ -14,7 +14,7 @@ func start_game():
 	get_tree().paused = false
 	# Only change level on the server.
 	# Clients will instantiate the level via the spawner.
-	if multiplayer.is_server() and not OS.has_environment("dedicated_server"):
+	if multiplayer.is_server() and not OS.has_feature("dedicated_server"):
 		change_level.call_deferred()
 
 # Call this function deferred and only on the main authority (server).
